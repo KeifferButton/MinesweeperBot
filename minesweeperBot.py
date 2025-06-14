2f446bfb71b58edcf32501c8c31fcb0535c21d28
iggmonster <112781295+iggmonster@users.noreply.github.com>
2025-06-14 02:44:54 -0400

    Finished getGameLocation
    Can now locate the game board and return it's coordinates

--- minesweeperBot.py
+++ minesweeperBot.py
@@ -1,14 +1,24 @@
+"""
+Designed to work on the following website:
+https://minesweeperonline.com/#beginner
+"""
+# Inner boarder + 6 pixels
+
 import pyautogui
 
 def main():
     print("Started")
-    scanForField()
+    board_coord = getGameLocation()
+    print(f"Target color found at {board_coord}")
 
 
-
-def scanForField():
+# Returns the top left corner of the game board
+# Specifically the first pixel on the top left which is
+# the specified TARGETCOLOR
+def getGameLocation():
     """TARGET COLOR:"""
     TARGETCOLOR = (189, 189, 189)
+    BACKGROUNDCOLOR = (255, 255, 255)
     
     # Get screen size
     screen_width, screen_height = pyautogui.size()
@@ -19,10 +29,16 @@ def scanForField():
     # Loop through every pixel (y then x) until target color is found
     for y in range(screen_height):
         for x in range(screen_width):
+            # Test for target color at pixel (x, y)
             if screenshot.getpixel((x, y)) == TARGETCOLOR:
-                print(f"Found target color at ({x}, {y})")
-                print(f"Adjacent colors are: UP: ({screenshot.getpixel((x, y - 1))}), LEFT: ({screenshot.getpixel((x - 1, y))}), DOWN: ({screenshot.getpixel((x, y + 1))}), RIGHT: ({screenshot.getpixel((x + 1, y))})")
-                return
+                # Test if down right pixel (x + 1, y + 1) is also target color
+                # and check if up left pixel (x - 1, y - 1) is background color
+                # to ensure pixel is actually from game board
+                if screenshot.getpixel((x+1, y+1)) == TARGETCOLOR and screenshot.getpixel((x - 1, y - 1)) == BACKGROUNDCOLOR:
+                    return (x, y)
+          
+    # Target color not found, return None   
+    return None
 
 
 if __name__=="__main__":
